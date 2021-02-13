9727ccc67328c57071e9634636d1c4b2ec9a67c6
Gravedigger7789 <Gravedigger7789@users.noreply.github.com>
2021-02-13 22:37:08 +0000

    deploy: Gatsby Site

--- sw.js
+++ sw.js
@@ -66,7 +66,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0ff3815379ec2a6fe4a2e8ad7964ef7f"
+    "revision": "2c4a25af7c78ed6f8f5c7c4ce66b7be2"
   },
   {
     "url": "static/webfonts/s/alegreyasans/v13/5aUt9_-1phKLFgshYDvh6Vwt7V9dv21T.woff2"
@@ -93,7 +93,7 @@ self.__precacheManifest = [
     "url": "static/webfonts/s/amaticsc/v15/TUZ3zwprpvBS1izr_vOMscGKfrUC.woff2"
   },
   {
-    "url": "webpack-runtime-06ab08aa26dbc4cab809.js"
+    "url": "webpack-runtime-a2e3163000d80817b501.js"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
