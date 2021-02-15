8a71408125dba198a7d99d3014f2add75144dcbd
Gravedigger7789 <Gravedigger7789@users.noreply.github.com>
2021-02-15 22:51:49 +0000

    deploy: Gatsby Site

--- sw.js
+++ sw.js
@@ -62,11 +62,11 @@ self.__precacheManifest = [
     "url": "framework-96f2a45b7f57ca14c9e2.js"
   },
   {
-    "url": "app-858c60f07046c9bc0493.js"
+    "url": "app-c744038178581e66e061.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "a338c96fc3b1f45d093572c7830628b7"
+    "revision": "70a51c9ac3b3ab5aabb266f0e07577bd"
   },
   {
     "url": "static/webfonts/s/alegreyasans/v13/5aUt9_-1phKLFgshYDvh6Vwt7V9dv21T.woff2"
@@ -93,7 +93,7 @@ self.__precacheManifest = [
     "url": "static/webfonts/s/amaticsc/v15/TUZ3zwprpvBS1izr_vOMscGKfrUC.woff2"
   },
   {
-    "url": "webpack-runtime-04919780dec93dffccd2.js"
+    "url": "webpack-runtime-5503a06047df19884d84.js"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -195,7 +195,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-858c60f07046c9bc0493.js`))) {
+  if (!resources || !(await caches.match(`/app-c744038178581e66e061.js`))) {
     return await fetch(event.request)
   }
 
